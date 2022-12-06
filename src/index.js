021afb19281c8b948bc19ee03adefe73fa1cec9f
MsNicoleta <106880559+MsNicoleta@users.noreply.github.com>
2022-12-06 11:47:51 +0000

    API Key / URL Added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+// Main temperature Function
+function displayTemperature(response) {
+    console.log(response.data.temperature.current);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = response.data.temperature.current;
+}
+let apiKey = `24b6dfe102oata11d30fb9f9e97168f3`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=24b6dfe102oata11d30fb9f9e97168f3&units=metric
+`;
+axios.get(apiUrl).then(displayTemperature);
+
+
 //alert("Hello World")
+  /*  function showUser(response) {
+        alert(`The user name is ${response.data.name}`);
+      } */
