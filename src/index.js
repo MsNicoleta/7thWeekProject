0611c08cbe4da9dd5cefca1ce11c9442e5bd21c2
MsNicoleta <106880559+MsNicoleta@users.noreply.github.com>
2022-12-06 12:34:26 +0000

    API updated
    API Data Updated

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 // Main temperature Function
 function displayTemperature(response) {
-    console.log(response.data.temperature.current);
+    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
+
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    cityElement.innerHTML = response.data.city;
+    descriptionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windElement.innerHTML = Math.round (response.data.wind.speed);
+    dateElement.innerHTML = response.data.time;
+    iconElement.innerHTML = response.data.condition.icon;
 }
 let apiKey = `24b6dfe102oata11d30fb9f9e97168f3`;
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=London&key=24b6dfe102oata11d30fb9f9e97168f3&units=metric
@@ -11,6 +24,6 @@ axios.get(apiUrl).then(displayTemperature);
 
 
 //alert("Hello World")
-  /*  function showUser(response) {
+  /*  function showUser(response) {s
         alert(`The user name is ${response.data.name}`);
       } */
